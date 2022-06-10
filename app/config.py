cae88966855130d5d3d4d479ec98e28dcee4afa0
beastmodepk <parthey.khanderia@gmail.com>
2022-06-10 13:57:24 -0400

    fixes #1

--- app/config.py
+++ app/config.py
@@ -1,11 +1,19 @@
 """
 App's config
 """
+import os
+from dotenv import load_dotenv
+
 class Config():
     """  Class storing apps's config   """
+
+    # secrets are stored in .env
+    load_dotenv()
+    openweathermap_api = os.environ.get("openweathermap_api")
+
     QUOTE_URL = "https://type.fit/api/quotes"
     WEATHER_FORCAST = {
-        "API_KEY": "fdfb08d6f28c8ca29b7880890bced87d",
+        "API_KEY": openweathermap_api,
         "URL": "https://api.openweathermap.org/data/2.5/forecast",
         "DEFAULT_COORDS": {"lat": "42.7653662","lon": "-71.467566" } # Nashua, NH
         #SAN_FRAN_COORDS: {"lat": "37.773972" ,"lon": "-122.431297"}
