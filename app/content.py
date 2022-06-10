0ef2b359b98882c150f91d4667abf1815e8a2595
Parthey Khanderia <pkhander@redhat.com>
2022-06-10 11:36:46 -0400

    add exception for weather_forcast +  variable name change: weather_app -> weather_format

--- app/content.py
+++ app/content.py
@@ -25,7 +25,7 @@ def get_random_quote():
     return random_quote
 
 
-def get_weather_forecast(coords=Config.WEATHER_APP['DEFAULT_COORDS']):
+def get_weather_forecast(coords=Config.WEATHER_FORCAST['DEFAULT_COORDS']):
     """
     get_weather_forecast(default coordinates for Nashua)
     - create weather url with coordinates
@@ -34,30 +34,31 @@ def get_weather_forecast(coords=Config.WEATHER_APP['DEFAULT_COORDS']):
     - return forcast
     """
     # Create weather_url
-    base_url = Config.WEATHER_APP['URL']
-    api_key = Config.WEATHER_APP['API_KEY']
+    base_url = Config.WEATHER_FORCAST['URL']
+    api_key = Config.WEATHER_FORCAST['API_KEY']
     weather_url = f"{base_url}?lat={coords['lat']}&lon={coords['lon']}&appid={api_key}&units=metric"
 
     # get response
-    response = requests.get(weather_url)
-    response.raise_for_status()
+    try:
+        response = requests.get(weather_url)
+        response.raise_for_status()
 
-    data = response.json()
+        data = response.json()
 
-    forecast = {'city': data['city']['name'], # city name
+        forecast = {'city': data['city']['name'], # city name
                 'country': data['city']['country'], # country name
                 'periods': list()} # list for storing forcast_data
 
-    # populate list with next 9 forecast periods
-    for period in data['list'][0:9]:
-        forecast['periods'].append({'timestamp': datetime.datetime.fromtimestamp(period['dt']),
-                                    'temp': round(period['main']['temp']),
-                                    'description': period['weather'][0]['description'].title()})
+        # populate list with next 9 forecast periods
+        for period in data['list'][0:9]:
+            forecast['periods'].append({'timestamp': datetime.datetime.fromtimestamp(period['dt']),
+                                        'temp': round(period['main']['temp']),
+                                        'description': period['weather'][0]['description'].title()})
 
-    return forecast
+        return forecast
+    except AttributeError as error:
+        print(error)
 
-# def get_twitter_trends():
-#     pass
 
 # def get_wikipedia_article():
 #     pass
