76d57e48266c6f7e8f5d38fa5ff35bf96f50f4c7
Parthey Khanderia <pkhander@redhat.com>
2022-06-10 11:33:43 -0400

    Added get_weather_forecast

--- app/config.py
+++ app/config.py
@@ -4,7 +4,10 @@ App's config
 class Config():
     """  Class storing apps's config   """
     QUOTE_URL = "https://type.fit/api/quotes"
-
-
-
+    WEATHER_APP = {
+        "API_KEY": "fdfb08d6f28c8ca29b7880890bced87d",
+        "URL": "https://api.openweathermap.org/data/2.5/forecast",
+        "DEFAULT_COORDS": {"lat": "42.7653662","lon": "-71.467566" } # Nashua, NH
+        #SAN_FRAN_COORDS: {"lat": "37.773972" ,"lon": "-122.431297"}
+    }
 Config = Config()
--- app/content.py
+++ app/content.py
@@ -2,9 +2,10 @@
 Content of the digest lives here
 """
 import random
+import datetime
 import requests
-import config
 from config import Config
+
 
 def get_random_quote():
     """
@@ -23,8 +24,37 @@ def get_random_quote():
             'quote': 'Life is on fire but I am in a pool'}]
     return random_quote
 
-# def get_weather_forecast():
-#     pass
+
+def get_weather_forecast(coords=Config.WEATHER_APP['DEFAULT_COORDS']):
+    """
+    get_weather_forecast(default coordinates for Nashua)
+    - create weather url with coordinates
+    - get response
+    - create list to store next 9 forcast periods
+    - return forcast
+    """
+    # Create weather_url
+    base_url = Config.WEATHER_APP['URL']
+    api_key = Config.WEATHER_APP['API_KEY']
+    weather_url = f"{base_url}?lat={coords['lat']}&lon={coords['lon']}&appid={api_key}&units=metric"
+
+    # get response
+    response = requests.get(weather_url)
+    response.raise_for_status()
+
+    data = response.json()
+
+    forecast = {'city': data['city']['name'], # city name
+                'country': data['city']['country'], # country name
+                'periods': list()} # list for storing forcast_data
+
+    # populate list with next 9 forecast periods
+    for period in data['list'][0:9]:
+        forecast['periods'].append({'timestamp': datetime.datetime.fromtimestamp(period['dt']),
+                                    'temp': round(period['main']['temp']),
+                                    'description': period['weather'][0]['description'].title()})
+
+    return forecast
 
 # def get_twitter_trends():
 #     pass
@@ -40,5 +70,7 @@ if __name__ == '__main__':
     print(f"Quote for the day is:\n{quote['text']}")
     print(f"Auther: {quote['author']}")
     print('*'*20)
+    san_fran = {"lat": "37.773972","lon": "-122.431297"}
+    url = get_weather_forecast()
+    print(url)
 #----------------------------------------------
-    pass
